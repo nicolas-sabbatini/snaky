6ffa39edb6184d815a62b68eb460f7db94fb6f0d
Nicolás Cesar Sabbatini Vrech <nik.code.things@gmail.com>
2022-01-28 01:00:30 -0300

    Snake movement

--- src/snake.rs
+++ src/snake.rs
@@ -1,77 +1,141 @@
 use super::arena::Position;
-use bevy::prelude::*;
+use bevy::{core::FixedTimestep, prelude::*};
 
 // Snake constant
 const HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 const BODY_COLOR: Color = Color::rgb(0.5, 0.5, 0.5);
 
+// Components and Bundles definitions
 #[derive(Component, Debug)]
 struct Head;
 
-// Movement status
+#[derive(Bundle)]
+struct HeadBundle {
+    head: Head,
+    position: Position,
+    movement_status: MovementStatus,
+    #[bundle]
+    sprite: SpriteBundle,
+}
+
 #[derive(Debug, PartialEq, Clone, Copy)]
-enum DirectionStatus {
+enum Direction {
     Left,
     Right,
     Up,
     Down,
 }
-impl Into<Position> for DirectionStatus {
+impl Direction {
+    fn opposite(&self) -> Direction {
+        match self {
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+        }
+    }
+}
+impl Into<Position> for Direction {
     fn into(self) -> Position {
         match self {
-            DirectionStatus::Left => Position { x: -1, y: 0 },
-            DirectionStatus::Right => Position { x: 1, y: 0 },
-            DirectionStatus::Up => Position { x: 0, y: -1 },
-            DirectionStatus::Down => Position { x: 0, y: 1 },
+            Direction::Left => Position { x: -1, y: 0 },
+            Direction::Right => Position { x: 1, y: 0 },
+            Direction::Up => Position { x: 0, y: -1 },
+            Direction::Down => Position { x: 0, y: 1 },
         }
     }
 }
 
-#[derive(Debug)]
-struct Direction(DirectionStatus);
+#[derive(Component, Debug)]
+struct MovementStatus {
+    current_direction: Direction,
+    next_direction: Direction,
+    buffer_direction: Option<Direction>,
+}
 
+// Plugin definition
 pub struct SnakePlugin;
 impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(Direction(DirectionStatus::Left));
-
         app.add_startup_system(spawn_head);
 
-        app.add_system(move_head);
+        app.add_system(
+            handle_input
+                .label(SnakeStages::Input)
+                .before(SnakeStages::Movement),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(0.5))
+                .with_system(move_snake.label(SnakeStages::Movement)),
+        );
     }
 }
 
+// System definitions
+#[derive(SystemLabel, Debug, Hash, PartialEq, Eq, Clone)]
+pub enum SnakeStages {
+    Input,
+    Movement,
+    Collision,
+    Growth,
+}
+
 fn spawn_head(mut commands: Commands) {
-    commands
-        .spawn_bundle(SpriteBundle {
+    commands.spawn_bundle(HeadBundle {
+        head: Head,
+        position: Position { x: 0, y: 0 },
+        movement_status: MovementStatus {
+            current_direction: Direction::Right,
+            next_direction: Direction::Right,
+            buffer_direction: None,
+        },
+        sprite: SpriteBundle {
             sprite: Sprite {
                 color: HEAD_COLOR,
                 custom_size: Some(Vec2::new(95.0, 95.0)),
                 ..Default::default()
             },
             ..Default::default()
-        })
-        .insert(Head)
-        .insert(Position { x: 0, y: 0 });
+        },
+    });
 }
 
-fn move_head(
-    mut query: Query<&mut Position, With<Head>>,
-    mut dir: ResMut<Direction>,
-    key_input: Res<Input<KeyCode>>,
-) {
-    if key_input.pressed(KeyCode::Left) && dir.0 != DirectionStatus::Right {
-        dir.0 = DirectionStatus::Left;
-    } else if key_input.pressed(KeyCode::Right) && dir.0 != DirectionStatus::Left {
-        dir.0 = DirectionStatus::Right;
-    } else if key_input.pressed(KeyCode::Up) && dir.0 != DirectionStatus::Down {
-        dir.0 = DirectionStatus::Up;
-    } else if key_input.pressed(KeyCode::Down) && dir.0 != DirectionStatus::Up {
-        dir.0 = DirectionStatus::Down;
+fn handle_input(mut query: Query<&mut MovementStatus, With<Head>>, key_input: Res<Input<KeyCode>>) {
+    let mut target_direction: Vec<Direction> = Vec::new();
+    if key_input.pressed(KeyCode::Left) {
+        target_direction.push(Direction::Left);
     }
-    let step_direction: Position = dir.0.into();
+    if key_input.pressed(KeyCode::Right) {
+        target_direction.push(Direction::Right);
+    }
+    if key_input.pressed(KeyCode::Up) {
+        target_direction.push(Direction::Up);
+    }
+    if key_input.pressed(KeyCode::Down) {
+        target_direction.push(Direction::Down);
+    }
 
-    for mut pos in query.iter_mut() {
-        //*pos = *pos + step_direction;
+    for td in target_direction.iter() {
+        for mut movement_status in query.iter_mut() {
+            if movement_status.current_direction.opposite() != *td {
+                movement_status.next_direction = *td;
+                movement_status.buffer_direction = None;
+            } else if movement_status.next_direction.opposite() != *td {
+                movement_status.buffer_direction = Some(*td);
+            }
+        }
+    }
+}
+
+fn move_snake(mut query: Query<(&mut MovementStatus, &mut Position), With<Head>>) {
+    for (mut ms, mut pos) in query.iter_mut() {
+        let step_direction: Position = ms.next_direction.into();
+        if let Some(d) = ms.buffer_direction {
+            ms.next_direction = d;
+            ms.buffer_direction = None;
+        }
+        ms.current_direction = ms.next_direction;
+        *pos = *pos + step_direction;
     }
 }
