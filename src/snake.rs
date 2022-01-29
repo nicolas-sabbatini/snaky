499a1402e5cf95860477fd9fbede6684a443109c
Nicolás Cesar Sabbatini Vrech <nik.code.things@gmail.com>
2022-01-28 22:43:25 -0300

    Snake body

--- src/snake.rs
+++ src/snake.rs
@@ -20,6 +20,20 @@ struct HeadBundle {
     sprite: SpriteBundle,
 }
 
+#[derive(Component, Debug)]
+struct BodyPart;
+
+#[derive(Bundle)]
+struct BodyPartBundle {
+    body_part: BodyPart,
+    position: Position,
+    #[bundle]
+    sprite: SpriteBundle,
+}
+
+#[derive(Component, Debug)]
+struct Last;
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 enum Direction {
     Left,
@@ -59,7 +73,8 @@ struct MovementStatus {
 pub struct SnakePlugin;
 impl Plugin for SnakePlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_head);
+        app.add_startup_system(spawn_head)
+            .add_startup_system(spawn_body);
 
         app.add_system(
             handle_input
@@ -86,7 +101,7 @@ pub enum SnakeStages {
 fn spawn_head(mut commands: Commands) {
     commands.spawn_bundle(HeadBundle {
         head: Head,
-        position: Position { x: 0, y: 0 },
+        position: Position { x: 1, y: 0 },
         movement_status: MovementStatus {
             current_direction: Direction::Right,
             next_direction: Direction::Right,
@@ -101,6 +116,23 @@ fn spawn_head(mut commands: Commands) {
             ..Default::default()
         },
     });
+}
+
+fn spawn_body(mut commands: Commands) {
+    commands
+        .spawn_bundle(BodyPartBundle {
+            body_part: BodyPart,
+            position: Position { x: 0, y: 0 },
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    color: BODY_COLOR,
+                    custom_size: Some(Vec2::new(CEL_SIZE * 0.75, CEL_SIZE * 0.75)),
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+        })
+        .insert(Last);
 }
 
 fn handle_input(mut query: Query<&mut MovementStatus, With<Head>>, key_input: Res<Input<KeyCode>>) {
@@ -130,14 +162,33 @@ fn handle_input(mut query: Query<&mut MovementStatus, With<Head>>, key_input: Re
     }
 }
 
-fn move_snake(mut query: Query<(&mut MovementStatus, &mut Position), With<Head>>) {
-    for (mut ms, mut pos) in query.iter_mut() {
-        let step_direction: Position = ms.next_direction.into();
-        if let Some(d) = ms.buffer_direction {
-            ms.next_direction = d;
-            ms.buffer_direction = None;
+fn move_snake(
+    mut query: QuerySet<(
+        QueryState<(&mut MovementStatus, &mut Position), With<Head>>,
+        QueryState<&mut Position, With<BodyPart>>,
+    )>,
+) {
+    let head_prev_pos: Position;
+    // Handle head movement
+    match query.q0().get_single_mut() {
+        Ok((mut head_ms, mut head_pos)) => {
+            head_prev_pos = (*head_pos).clone();
+            // Move head
+            let step_direction: Position = head_ms.next_direction.into();
+            if let Some(d) = head_ms.buffer_direction {
+                head_ms.next_direction = d;
+                head_ms.buffer_direction = None;
+            }
+            head_ms.current_direction = head_ms.next_direction;
+            *head_pos = *head_pos + step_direction;
         }
-        ms.current_direction = ms.next_direction;
-        *pos = *pos + step_direction;
+        Err(_) => panic!("HOW DID WE EVEN GET HERE!?!?"),
+    };
+    // Handle body movement
+    // Create a vector to save positions to move the body
+    let mut nex_body_pos: Vec<Position> = query.q1().iter().skip(1).map(|p| *p).collect();
+    nex_body_pos.push(head_prev_pos);
+    for (mut bp, next) in query.q1().iter_mut().zip(nex_body_pos.iter()) {
+        *bp = *next;
     }
 }
